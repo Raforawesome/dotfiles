5cc3821cf3676594200c078a83b60cd11cb133ea
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-12-03 12:04:30 -0500

    Added symbols for warnings/errors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,11 +14,10 @@ vim.cmd("set smartcase")      -- Make searches only case-sensitive if you use a 
 -- noselect: Do not auto-select, nvim-cmp plugin will handle this for us.
 vim.o.completeopt = "menuone,noselect"
 
-local signs = { error = "", warn = "", hint = "", info = " " }
-for type, icon in pairs(signs) do
-	local hl = "diagnosticsign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
 
 
 
