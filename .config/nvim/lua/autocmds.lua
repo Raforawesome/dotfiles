27439afd9f08ea4f7c7a18475f4ec23b0443eea8
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-01-13 01:54:15 -0500

    force commit new config

---
+++ .config/nvim/lua/autocmds.lua
@@ -0,0 +1,17 @@
+local module = nil -- make it clear this file is a module
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+return module
