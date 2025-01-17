4980f7a38b69eeafdb2c14abf2b19cd9073fffa2
Raforawesome <raforawesome@raforaweso.me>
2025-01-17 09:01:46 -0500

    added more fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,5 +7,14 @@ if status is-interactive
     end
 end
 
+###########
+# ALIASES #
+###########
+alias py=python3
+alias bohr-curl="curl -u \"chau2962:qCg&QUDDWT*2K7\" -Ss"
+alias 264-build="clang -c src/*.c && clang -o main *.o && rm *.o"
+alias 264-test="clang -c src/*.c && clang -o main *.o && rm *.o && ./main"
+alias 264-run="clang -c src/*.c && clang -o main *.o && rm *.o && ./main && rm main"
+alias neofetch=fastfetch
 alias neofetch=fastfetch
 
